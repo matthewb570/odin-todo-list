01485a9a85e42ca0ab66b88b18452bfeb6e99631
Matthew Burgess <boog397@gmail.com>
2024-06-24 22:34:09 -0500

    Add todo class

---
+++ src/js/todo.js
@@ -0,0 +1,11 @@
+class Todo {
+    constructor(title, description, dueDate, priority, isDone) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.isDone = isDone;
+    }
+}
+
+export default Todo;
