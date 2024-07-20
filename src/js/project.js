9d590bdea9914bfb4e2e7b8d80a278f9d621e3b5
Matthew Burgess <boog397@gmail.com>
2024-07-20 14:18:00 -0500

    Add helper methods and ids to Todo and Project classes

--- src/js/project.js
+++ src/js/project.js
@@ -1,7 +1,19 @@
+import { v4 as uuidv4 } from 'uuid';
+
 class Project {
     constructor(title, todos) {
+        this.id = uuidv4();
         this.title = title;
         this.todos = todos;
+    }
+
+    addTodo(todo) {
+        this.todos.push(todo);
+    }
+
+    removeTodo(id) {
+        let indexToRemove = this.todos.findIndex(todo => todo.id === id);
+        this.todos.splice(indexToRemove, 1);
     }
 }
 
--- src/js/todo.js
+++ src/js/todo.js
@@ -1,10 +1,17 @@
+import { v4 as uuidv4 } from 'uuid';
+
 class Todo {
     constructor(title, description, dueDate, priority, isDone) {
+        this.id = uuidv4();
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
         this.isDone = isDone;
+    }
+
+    reverseIsDone() {
+        this.isDone = !this.isDone;
     }
 }
 
